f7e1a7b0044355692d441d19f0c5334deef11efa
PratGit1606 <pratham.hegde2005@gmail.com>
2024-12-29 04:09:04 -0700

    First

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import AnimatedCursor from './components/AnimatedCursor'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Pratham Hegde - Portfolio',
+  description: 'Software Developer | Researcher | Student',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-black text-white`}>{children}</body>
+    </html>
+  )
+}
+
